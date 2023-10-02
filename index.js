0e53f408566742a552c26fa9a6cca3499739511a
Ahmed Ibrahim Ahmed <ahmed7cf@gmail.com>
2023-10-02 14:18:08 +0300

    done

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Code your solutions in this file
+
+let newlist = []
+function writeCards(arr, gift) {
+    for(let i = 0; i < arr.length; i++){
+        newlist.push("Thank you, " + arr[i] + ", for the wonderful " +  gift + " gift!");
+        console.log(newlist[i]);
+    }
+    return newlist;
+}
+
+function countDown(number) {
+    while (number >= 0) {
+        console.log(number)
+        number--;
+    }
+}
